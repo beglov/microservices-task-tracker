2c406c3d61c9dfa822370f56a1e47e1148ef755b
Beglov Sergey <beglov.sergey@yandex.ru>
2022-08-20 14:19:11 +0300

    Add devise

--- tasks/config/routes.rb
+++ tasks/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :accounts
   root "tasks#index"
   resources :tasks
 end
--- tasks/db/schema.rb
+++ tasks/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_13_120007) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_20_105806) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_13_120007) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.enum "role", default: "worker", null: false, enum_type: "account_roles"
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_accounts_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
   end
 
   create_table "tasks", force: :cascade do |t|
