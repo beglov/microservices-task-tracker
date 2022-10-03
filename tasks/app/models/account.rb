899c23ad9f65357bd7c6c7fca9bb9e913817e5b3
Beglov Sergey <beglov.sergey@yandex.ru>
2022-10-03 16:41:30 +0300

    Init analytics service
    Run command: rails new . --force --database=postgresql

--- tasks/app/models/account.rb
+++ tasks/app/models/account.rb
@@ -6,11 +6,12 @@ class Account < ApplicationRecord
     Rails.logger.debug "=" * 80
     Rails.logger.debug auth
     Rails.logger.debug "=" * 80
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |account|
-      account.public_id = auth.info.public_id
+    where(public_id: auth.info.public_id).first_or_create do |account|
       account.full_name = auth.info.full_name
       account.email = auth.info.email
       account.role = auth.info.role
+      account.provider = auth.provider
+      account.uid = auth.uid
     end
   end
 
