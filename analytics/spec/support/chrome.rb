b53fda188e8a02271003f966077a861aa11fda62
Beglov Sergey <beglov.sergey@yandex.ru>
2022-10-05 12:27:54 +0300

    Tests in docker

--- analytics/spec/support/chrome.rb
+++ analytics/spec/support/chrome.rb
@@ -1,23 +1,13 @@
-# RSpec.configure do |config|
-#   config.before(:each, type: :system) do
-#     if ENV["SHOW_BROWSER"] == "true"
-#       driven_by :selenium_chrome
-#     else
-#       driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
-#     end
-#   end
-# end
+# https://nicolasiensen.github.io/2022-03-11-running-rails-system-tests-with-docker/
 
-# https://avdi.codes/run-rails-6-system-tests-in-docker-using-a-host-browser/
-
-Capybara.server_host = ENV.fetch("CAPYBARA_SERVER_HOST")
-Capybara.server_port = ENV.fetch("CAPYBARA_SERVER_PORT")
+Capybara.server_host = "0.0.0.0"
+Capybara.app_host = "http://#{Socket.gethostname}:#{Capybara.server_port}"
 
 RSpec.configure do |config|
   config.before :each, type: :system do
-    driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400], options: {
+    driven_by :selenium, using: ENV.fetch("TEST_BROWSER").to_sym, screen_size: [1400, 1400], options: {
       browser: :remote,
-      url: "http://host.docker.internal:9515",
+      url: ENV.fetch("TEST_BROWSER_URL"),
     }
   end
 end
