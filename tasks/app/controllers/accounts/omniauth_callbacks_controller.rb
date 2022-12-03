77e1d07bca8c0840a2b589346ddb9ce6502915da
Beglov Sergey <beglov.sergey@yandex.ru>
2022-12-03 16:52:30 +0300

    Auth service

--- tasks/app/controllers/accounts/omniauth_callbacks_controller.rb
+++ tasks/app/controllers/accounts/omniauth_callbacks_controller.rb
@@ -1,10 +1,10 @@
 module Accounts
   class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     def doorkeeper
-      @account = Account.from_omniauth(request.env["omniauth.auth"])
+      result = AuthService.new(request.env["omniauth.auth"]).call
 
-      if @account.persisted?
-        sign_in_and_redirect @account, event: :authentication
+      if result.success?
+        sign_in_and_redirect result.success, event: :authentication
         set_flash_message(:notice, :success, kind: "Doorkeeper") if is_navigational_format?
       else
         session["devise.doorkeeper_data"] = request.env["omniauth.auth"]
--- tasks/app/models/account.rb
+++ tasks/app/models/account.rb
@@ -2,19 +2,6 @@ class Account < ApplicationRecord
   devise :database_authenticatable,
          :omniauthable, omniauth_providers: %i[doorkeeper]
 
-  def self.from_omniauth(auth)
-    Rails.logger.debug "=" * 80
-    Rails.logger.debug auth
-    Rails.logger.debug "=" * 80
-    where(public_id: auth.info.public_id).first_or_create do |account|
-      account.full_name = auth.info.full_name
-      account.email = auth.info.email
-      account.role = auth.info.role
-      account.provider = auth.provider
-      account.uid = auth.uid
-    end
-  end
-
   def worker?
     role == "worker"
   end
