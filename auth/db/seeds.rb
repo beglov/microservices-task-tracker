5e0287b1ec3ecca2f131470468e13ecd139ef0b0
Beglov Sergey <beglov.sergey@yandex.ru>
2023-11-02 17:27:04 +0300

    Seeds auth doorkeeper application

--- auth/db/seeds.rb
+++ auth/db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Doorkeeper::Application.create!(
+  {
+    name: "MyAuth",
+    uid: "Vc4RA6fYjCcOjjaTWcj-XZDyBMEm3Jbzs6ov_Q55hYQ",
+    secret: "Zv2RqMbS8ijWt9_aeuP2z0NYhnXHM0pEJyGAansxcPk",
+    redirect_uri: "http://localhost:3010/accounts/auth/doorkeeper/callback\r\nhttp://localhost:3020/accounts/auth/doorkeeper/callback\r\nhttp://localhost:3030/accounts/auth/doorkeeper/callback",
+    scopes: "read",
+    confidential: true,
+  },
+)
